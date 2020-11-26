9f443665a7f3f18bc45a38db92369c8ef09a8a53
hadi <hadivardanjani1378@gmail.com>
2020-11-26 12:20:32 +0330

    create project

---
+++ index.js
@@ -0,0 +1,52 @@
+let ToDoList=[]
+function AddTask() {
+  let inputValue = document.getElementById("text-input").value;
+  inputValue = inputValue.trim();
+  if (inputValue.length >= 1) {
+    ToDoList.push({title: inputValue, id: Date.now(), done:false})
+  }
+  read()
+  inputClear()
+}
+
+function read(){
+  clear();
+  ToDoList.map((item)=>{
+    addTodoBox(item.title,item.id,item.done)
+  })
+}
+
+function clear(){
+  let container = document.getElementById('todo-box-container')
+  container.innerHTML=""
+}
+
+function inputClear(){
+  let container = document.getElementById('text-input')
+  container.value=""
+}
+
+// console.log(ToDoList)
+function addTodoBox(title, id, done) {
+  let container = document.getElementById("todo-box-container");
+  container.innerHTML += `
+  <div class="todo-box-container pb-2">
+     <div
+       class=" border border-dark todo-box d-flex justify-content-between align-items-center px-2"
+     >
+       <span onclick="done(${id})" class="${done && "done-task"}"> ${title} </span>
+       <i onclick="deleteTask(${id})" class="material-icons">delete</i>
+     </div>
+   </div>`;
+}
+
+function done(id){
+  let task = ToDoList.find((i)=> i.id===id)
+  task.done = true
+  read()
+}
+
+function deleteTask(id){
+  ToDoList = ToDoList.filter((i)=>i.id !== id)
+  read()
+}
